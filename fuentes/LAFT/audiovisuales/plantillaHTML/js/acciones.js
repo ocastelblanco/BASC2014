99e81f88b6c884139d6ae8e6b45b4d435bb547e8
ocastelblanco <ocastelblanco@gmail.com>
2014-04-25 20:19:59 -0500

    Ajuste para pantallas de menos de 768px

--- fuentes/LAFT/audiovisuales/plantillaHTML/js/acciones.js
+++ fuentes/LAFT/audiovisuales/plantillaHTML/js/acciones.js
@@ -1,1 +1,15 @@
-/* Para aplicar según sea necesario */
+/*  */
+$(function(){
+    ajustarMenu();
+    window.onresize = ajustarMenu;
+    $('#abrirMenu').click(function() {
+        $('.vinculos').toggle("slide", {direction: "up", easing:"easeInQuint"}, 'slow');
+    });
+});
+function ajustarMenu(){
+    if(window.innerWidth < 768){
+        $('.vinculos').hide("slide", {direction: "up", easing:"easeInQuint"}, 'slow');
+    } else {
+        $('.vinculos').show();
+    }
+}
