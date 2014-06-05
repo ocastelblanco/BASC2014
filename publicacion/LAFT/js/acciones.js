fe7f6e69cb1d4e0123e5ad88c73abf7f4d181800
ocastelblanco <ocastelblanco@gmail.com>
2014-06-05 18:35:37 -0500

    Pagina 2 del modulo 2, incluye raphael.js

--- publicacion/LAFT/js/acciones.js
+++ publicacion/LAFT/js/acciones.js
@@ -24,6 +24,7 @@ if (!Array.prototype.indexOf) {
     };
   }
 /* Ahora si, el código de verdad */
+var lienzoArbol;
 $(function(){
     ajustarMenu();
     window.onresize = ajustarMenu;
@@ -45,12 +46,68 @@ $(function(){
         'trigger': 'hover'
     }).prepend('<i class="fa fa-chevron-circle-up"></i><br>');
     $('a.bocadillo').tooltip({'container': 'body'}).click(function(evento) {evento.preventDefault();});
+    $('.arbol .rama').each(function(index) {
+        var margen = ($(this).parent().parent().height()-$(this).outerHeight(true))/2;
+        $(this).css('margin-top', margen+'px');
+    });
+    $('.arbol a').click(function(evento) {
+        evento.preventDefault();
+    }).popover({
+        'container': 'body',
+        'placement': 'auto top',
+        'trigger': 'click'
+    });
+    if ($('.arbol').length > 0) {
+        alistaArbol();
+    }
+    $('.mapamundi a').click(function(evento) {
+        evento.preventDefault();
+    }).popover({
+        'container': 'body',
+        'placement': 'auto top',
+        'trigger': 'click',
+        'html': true
+    });
 });
+function alistaArbol() {
+    if (lienzoArbol) {
+        lienzoArbol.remove();
+        lienzoArbol = null;
+    }
+    lienzoArbol = Raphael('graficaArbol',$('#graficaArbol').width(),$('#graficaArbol').height());
+    var nivel1 = $('.rama')[0];
+    var pos1x = $(nivel1).outerWidth(true);
+    var pos1y = $(nivel1).position().top+Number($(nivel1).css('margin-top').slice(0,-2))+($(nivel1).outerHeight(false)/2);
+    var yGral = $('.arbol').offset().top;
+    var color = $(nivel1).css('border-color');
+    $(nivel1).parent().next().children('.row').children('div:first-of-type').children('.rama').each(function(index){
+        var pos2y = ($(this).offset().top-yGral)+($(this).outerHeight(false)/2);
+        var linea = lienzoArbol.path('M'+pos1x+','+pos1y+'L'+(pos1x+15)+','+pos1y+'L'+(pos1x+15)+','+pos2y+'L'+(pos1x+35)+','+pos2y);
+        linea.attr('stroke', color);
+        linea.attr('stroke-width', '2');
+        dibujaLineasArbol($(this),'.arbol',lienzoArbol);
+    });
+}
+function dibujaLineasArbol(origen,padre,papel) {
+    var pos1x = $(origen).offset().left-$(padre).offset().left+$(origen).outerWidth();
+    var pos1y = $(origen).offset().top-$(padre).offset().top+($(origen).outerHeight()/2);
+    var color = $(origen).css('border-color');
+    $(origen).parent().next().children('a').each(function(index){
+        var pos2y = ($(this).offset().top-$(padre).offset().top)+($(this).outerHeight(false)/2);
+        var linea = papel.path('M'+pos1x+','+pos1y+'L'+(pos1x+15)+','+pos1y+'L'+(pos1x+15)+','+pos2y+'L'+(pos1x+35)+','+pos2y);
+        linea.attr('stroke', color);
+        linea.attr('stroke-width', '2');
+        dibujaLineasArbol($(this),padre,papel);
+    });
+}
 function ajustarMenu(){
     if(window.innerWidth < 768){
         $('.vinculos').hide("slide", {direction: "up", easing:"easeInQuint"}, 'slow');
     } else {
         $('.vinculos').show();
+    }
+    if ($('.arbol').length > 0) {
+        alistaArbol();
     }
 }
 function alistaPresaberes() {
